d64f2b939c1d74b1b5e6dfaca34dd19a06d24ad8
Jakub Serok <jakub.serok@wp.pl>
2024-04-07 22:11:45 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import axios from "axios";
 
 function App() {
   const [coins, setCoins] = useState([]);
-
   const url =
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true/ping?x_cg_demo_api_key=CG-GqpLHgrRq8GCDhtDhYAYxWNw";
 
